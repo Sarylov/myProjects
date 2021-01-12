eb47ba1ae4a0113e17666114225fc17cf0b383f6
Sarylov <77244095+Sarylov@users.noreply.github.com>
2021-01-12 21:18:42 +0300

    add test task

--- testTask/js/script.js
+++ testTask/js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    $('.header-slider').slick();
+    $('.header-slider').slick({
+        prevArrow: "<img src='../img/LeftArrow.png' class='prev' alt='1'>",
+        nextArrow: "<img src='../img/RightArrow.png' class='next' alt='2'>",
+    });
+    
 })
